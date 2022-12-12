b1623f0456f39592311c5d1e7c03a39a40cebd30
black9807 <nic1200182@gmail.com>
2022-12-12 11:53:37 +0100

    babbi-natale

---
+++ spring-babbi-natale/src/main/java/com/nicolo/SpringBabbiNataleApplication.java
@@ -0,0 +1,13 @@
+package com.nicolo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBabbiNataleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBabbiNataleApplication.class, args);
+	}
+
+}
---
+++ spring-babbi-natale/src/test/java/com/nicolo/SpringBabbiNataleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicolo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBabbiNataleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
