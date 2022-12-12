c3bb7c24207fc1c3382ee3932eea58ee78f715da
black9807 <nic1200182@gmail.com>
2022-12-12 20:47:24 +0100

    super rollback

--- spring-babbi-natale/src/main/java/com/nicolo/entities/Consegna.java
+++ spring-babbi-natale/src/main/java/com/nicolo/entities/Consegna.java
@@ -28,12 +28,94 @@ public class Consegna {
 	@JoinColumn(name = "sacco_id")
 	private Sacco sacco;
 	
-	private Bimbo bimbo;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "dono_id")
+	private Dono dono;
+	private int quantita;
 	
 	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "utente_id")
-	private Utente utente;
+	@JoinColumn(name = "bimbo_id")
+	private Bimbo bimbo;
 	
+	@Column(columnDefinition = "DATE", name = "consegnato_data")
+	@DateTimeFormat(iso = ISO.DATE)
+	private LocalDate consegnatoData;
 	
+	@Column(columnDefinition = "TIME", name = "consegnato_ora")
+	@DateTimeFormat(iso = ISO.TIME)
+	private LocalTime consegnatoOra;
+	@Column(columnDefinition = "int default 0")
+	private int annullato; // 0 = no (default) | 1 = si
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public Sacco getSacco() {
+		return sacco;
+	}
+	
+	public void setSacco(Sacco sacco) {
+		this.sacco = sacco;
+	}
+	
+	public Dono getDono() {
+		return dono;
+	}
+	
+	public void setDono(Dono dono) {
+		this.dono = dono;
+	}
+	
+	public int getQuantita() {
+		return quantita;
+	}
+	
+	public void setQuantita(int quantita) {
+		this.quantita = quantita;
+	}
+	
+	public Bimbo getBimbo() {
+		return bimbo;
+	}
+	
+	public void setBimbo(Bimbo bimbo) {
+		this.bimbo = bimbo;
+	}
+	
+	public LocalDate getConsegnatoData() {
+		return consegnatoData;
+	}
+	
+	public void setConsegnatoData(LocalDate consegnatoData) {
+		this.consegnatoData = consegnatoData;
+	}
+	
+	public LocalTime getConsegnatoOra() {
+		return consegnatoOra;
+	}
+	
+	public void setConsegnatoOra(LocalTime consegnatoOra) {
+		this.consegnatoOra = consegnatoOra;
+	}
+	
+	public int getAnnullato() {
+		return annullato;
+	}
+	
+	public void setAnnullato(int annullato) {
+		this.annullato = annullato;
+	}
+	
+	@Override
+	public String toString() {
+		return "Consegna [id=" + id + ", sacco=" + sacco + ", dono=" + dono + ", quantita=" + quantita + ", bimbo="
+				+ bimbo + ", consegnatoData=" + consegnatoData + ", consegnatoOra=" + consegnatoOra + ", annullato="
+				+ annullato + "]";
+	}
 	
 }
