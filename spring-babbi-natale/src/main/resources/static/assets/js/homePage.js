191352ee54d43153222a392ec3eb28a7ae2724cc
rottenapple-tech <dam.cyb.act@gmail.com>
2022-12-13 12:55:32 +0100

    update

--- spring-babbi-natale/src/main/resources/static/assets/js/homePage.js
+++ spring-babbi-natale/src/main/resources/static/assets/js/homePage.js
@@ -1,6 +1,20 @@
 const cardTitle = document.querySelector('.card-title');
+const cardText = document.querySelector('.card-text');
 
 var nomeUtente = document.querySelector('#username-utente');
 var tipoUtente = document.querySelector('#tipo-utente').getAttribute('value');
 
+/* ---------------------------- END VARs & CONSTs --------------------------- */
+
 cardTitle.textContent=nomeUtente.getAttribute('value');
+
+window.addEventListener('load',()=>{
+    console.log(tipoUtente);
+    console.log(typeof tipoUtente);
+
+    if(tipoUtente == 'admin'){
+        cardText.textContent='amministratore';
+    }else{
+        cardText.textContent='babbo natale';
+    }
+})
