d8eeb22c9445b4f3bdba17065bea38f1c6471193
rottenapple-tech <dam.cyb.act@gmail.com>
2022-12-13 14:19:54 +0100

    add

--- spring-babbi-natale/target/classes/static/assets/js/homePage.js
+++ spring-babbi-natale/target/classes/static/assets/js/homePage.js
@@ -1,5 +1,8 @@
 const cardTitle = document.querySelector('.card-title');
 const cardText = document.querySelector('.card-text');
+const buttonsAdmin = document.querySelectorAll('#btn-admin');
+const buttonSanta = document.querySelector('#btn-santa');
+const ul = document.querySelector('#list-btn');
 
 var nomeUtente = document.querySelector('#username-utente');
 var tipoUtente = document.querySelector('#tipo-utente').getAttribute('value');
@@ -8,13 +11,40 @@ var tipoUtente = document.querySelector('#tipo-utente').getAttribute('value');
 
 cardTitle.textContent=nomeUtente.getAttribute('value');
 
-window.addEventListener('load',()=>{
-    console.log(tipoUtente);
-    console.log(typeof tipoUtente);
 
+function changeCard(){
     if(tipoUtente == 'admin'){
         cardText.textContent='amministratore';
     }else{
         cardText.textContent='babbo natale';
     }
+}
+
+function changeNav(){
+    if(tipoUtente == 'utente'){
+        buttonsAdmin.forEach(button=>{
+            button.setAttribute('hidden','')
+        });
+
+        buttonSanta.removeAttribute('hidden');
+       
+        changeClass();
+
+    }else{
+        null
+    }
+}
+
+function changeClass(){
+    ul.classList.remove('justify-content-evenly')
+    ul.classList.add('justify-content-center')
+
+    buttonSanta.classList.add('ms-5');
+}
+
+/* ------------------------------ END FUNCTIONs ----------------------------- */
+
+window.addEventListener('load',()=>{
+    changeCard();
+    changeNav();  
 })
