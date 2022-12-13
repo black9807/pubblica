34b6a7d3f1c48404f4658465c3d97ff30ec992df
black9807 <nic1200182@gmail.com>
2022-12-13 16:11:18 +0100

    loginCheck get redirect

--- spring-babbi-natale/src/main/java/com/nicolo/presentation/UtentiMVC.java
+++ spring-babbi-natale/src/main/java/com/nicolo/presentation/UtentiMVC.java
@@ -2,6 +2,7 @@ package com.nicolo.presentation;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -36,4 +37,6 @@ public class UtentiMVC {
 		
 	}
 	
+	@GetMapping("loginCheck")
+	public String redirect() { return "redirect:/"; }
 }
