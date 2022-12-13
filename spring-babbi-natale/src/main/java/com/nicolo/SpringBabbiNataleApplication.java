dcc56002fc02b626fe3795774977e56da65cc73d
rottenapple-tech <dam.cyb.act@gmail.com>
2022-12-13 15:30:09 +0100

    ciao

--- spring-babbi-natale/src/main/java/com/nicolo/SpringBabbiNataleApplication.java
+++ spring-babbi-natale/src/main/java/com/nicolo/SpringBabbiNataleApplication.java
@@ -1,13 +1,28 @@
 package com.nicolo;
 
+import java.beans.BeanProperty;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.nicolo.dal.*;
+import com.nicolo.entities.*;
 
 @SpringBootApplication
 public class SpringBabbiNataleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBabbiNataleApplication.class, args);
+	}
+
+	@Bean
+	public static void add(BimbiDAO bDao, DoniDAO dDAO){
+		return a->{
+		Bimbo b = new Bimbo();
+		b.setId(1);
+		b.setNome("Niccolo");
+		b.setIndirizzo("via Mar Cinese,122");
+		b.setDoni() 	
+		}
 	}
 
 }
