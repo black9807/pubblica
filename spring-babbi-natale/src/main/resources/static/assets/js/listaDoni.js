15b2b0007a0e7fcaf7699ab4ad17138cf6625f57
rottenapple-tech <dam.cyb.act@gmail.com>
2022-12-13 16:00:27 +0100

    ciao

--- spring-babbi-natale/src/main/resources/static/assets/js/listaDoni.js
+++ spring-babbi-natale/src/main/resources/static/assets/js/listaDoni.js
@@ -1,5 +1,18 @@
 import{cardNameValue} from "../js/homePage";
 
 const cardTitle = document.querySelector('.card-title');
+const row = document.querySelector('#dono-info');
+const url = "http://localhost:9013/api/test";
 
 cardNameValue(cardTitle);
+
+()=>{
+    fetch(url)
+    .then(data=>{return data.json()})
+    .then(resp=>{
+        resp.forEach(element => {
+            console.log(element);
+        });
+
+    })
+}
---
+++ spring-babbi-natale/target/classes/static/assets/js/listaDoni.js
@@ -0,0 +1,11 @@
+import{cardNameValue} from "../js/homePage";
+
+const cardTitle = document.querySelector('.card-title');
+const row = document.querySelector('#dono-info');
+const ulr = "localhost:9013/api/doni";
+
+cardNameValue(cardTitle);
+
+()=>{
+    fetch
+}
