bedcdc16b08060096a6f40ecccca7d03d003eb5e
black9807 <nic1200182@gmail.com>
2022-12-13 16:01:04 +0100

    doni mvc

---
+++ spring-babbi-natale/src/main/java/com/nicolo/presentation/DoniMVC.java
@@ -0,0 +1,23 @@
+package com.nicolo.presentation;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("doni")
+public class DoniMVC {
+	
+	@GetMapping("listaDoni")
+	public String listaDoni(HttpSession session) {
+		
+		if (session.getAttribute("loggedUser") == null)
+			return "redirect:/";
+		
+		return "listaDoni";
+		
+	}
+	
+}
