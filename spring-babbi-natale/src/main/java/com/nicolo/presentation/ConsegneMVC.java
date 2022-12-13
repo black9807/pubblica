be0fd2833b444b7757b39c5056d40ce07aa921c8
black9807 <nic1200182@gmail.com>
2022-12-13 15:53:26 +0100

    .

---
+++ spring-babbi-natale/src/main/java/com/nicolo/presentation/ConsegneMVC.java
@@ -0,0 +1,12 @@
+package com.nicolo.presentation;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("consegne")
+public class ConsegneMVC {
+
+	
+	
+}
