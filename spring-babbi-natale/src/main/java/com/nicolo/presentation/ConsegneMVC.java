1ba9943165136271b7d61f9fef29b49bf3dfa649
black9807 <nic1200182@gmail.com>
2022-12-15 12:05:49 +0100

    report admin

--- spring-babbi-natale/src/main/java/com/nicolo/presentation/ConsegneMVC.java
+++ spring-babbi-natale/src/main/java/com/nicolo/presentation/ConsegneMVC.java
@@ -1,12 +1,22 @@
 package com.nicolo.presentation;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("consegne")
 public class ConsegneMVC {
 
-	
+	@GetMapping("report")
+	public String reportAdmin(HttpSession session) {
+		
+		if (session.getAttribute("loggedUser") == null)
+			return "redirect:/";
+		
+		return "report";
+	}
 	
 }
