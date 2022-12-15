e527d7b89b3c03867f416d0861ee9b81e10e1e66
rottenapple-tech <dam.cyb.act@gmail.com>
2022-12-15 10:06:30 +0100

    commit

--- spring-babbi-natale/target/classes/static/assets/js/addDoni.js
+++ spring-babbi-natale/target/classes/static/assets/js/addDoni.js
@@ -1,8 +1,10 @@
 const cardTitle = document.querySelector('.card-title');
 
-const row = document.querySelector('#infos');
+const body = document.querySelector('#infos');
 
 const urlBimbo = "http://localhost:9013/api/bimbi";
+
+const urlSacchi = "http://localhost:9013/api/sacchi"
 
 
 
@@ -13,19 +15,42 @@ function cardNameValue(){
 
 cardNameValue();
 
+    var row = document.createElement('tr');
 
     fetch(urlBimbo)
     .then(data=>{return data.json()})
     .then(resp=>{
         resp.forEach(element => {
-            console.log(element);
-        //     var td = document.createElement('td');
-        //     td.textContent = element.nome;
-        //     var td2 = document.createElement('td');
-        //     td2.textContent = element.disponibilita;
+            
+            var td1 = document.createElement('td');
+            var td2 = document.createElement('td');
 
-        //     row.appendChild(td);
-        //     row.appendChild(td2);
+            td1.textContent = element.nome;
+            
+            element.doni.forEach(element=>{
+                var nomeDono = element.nome;
+                td2.textContent = nomeDono;
+            })
+
+            row.appendChild(td1);
+            row.appendChild(td2);
+
          });
+
+
+
+    fetch(urlSacchi)
+    .then(data=>{return data.json()})
+    .then(resp=>{
+        resp.forEach(element=>{
+            var nome = element.nome;
+            var id = element.id;
+
+            var td3 = document.createElement('td');
+            td3.textContent= nome+"-"+id;
+        })
+
+
+    })
 
 })
