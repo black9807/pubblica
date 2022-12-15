218017706bb00a7c2acdfe28ed8eef1b30c0c494
black9807 <nic1200182@gmail.com>
2022-12-15 11:44:37 +0100

    unused changed

--- spring-babbi-natale/src/main/java/com/nicolo/presentation/DoniMVC.java
+++ spring-babbi-natale/src/main/java/com/nicolo/presentation/DoniMVC.java
@@ -3,17 +3,9 @@ package com.nicolo.presentation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import com.nicolo.dal.BimbiDAO;
-import com.nicolo.dal.ConsegneDAO;
 import com.nicolo.dal.DoniDAO;
-import com.nicolo.dal.SacchiDAO;
-import com.nicolo.entities.Bimbo;
-import com.nicolo.entities.Consegna;
-import com.nicolo.entities.Sacco;
 
 import jakarta.servlet.http.HttpSession;
 
