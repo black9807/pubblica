8658422a3409582cd77a0aee7e223f3714495822
rottenapple-tech <dam.cyb.act@gmail.com>
2022-12-15 12:43:51 +0100

    fetch doni

--- spring-babbi-natale/src/main/resources/static/assets/js/addSacchi.js
+++ spring-babbi-natale/src/main/resources/static/assets/js/addSacchi.js
@@ -1,5 +1,34 @@
 const cardTitle = document.querySelector('.card-title');
+const body = document.querySelector('#infos');
+const urlSacchi = "http://localhost:9013/api/sacchi"
+const urlBabbi = "http://localhost:9013/api/utentiByTipo/utente"
+
+
+var td2= document.createElement('td');
+var td3 = document.createElement('td');
 
 function cardNameValue(){
     cardTitle.textContent=sessionStorage.getItem('username');
 }
+
+function fillTable(){
+    
+    fetch(urlBabbi)
+    .then(data=>{return data.json()})
+    .then(resp=>{
+        resp.forEach(element => {
+            var row = document.createElement('tr');
+            var td1 = document.createElement('td');
+            var nome = element.nome;
+            td1.textContent = nome;
+            row.appendChild(td1);
+            body.appendChild(row)
+        });
+    })
+
+   
+
+}
+
+fillTable()
+cardNameValue()
--- spring-babbi-natale/target/classes/static/assets/js/addDoni.js
+++ spring-babbi-natale/target/classes/static/assets/js/addDoni.js
@@ -1,19 +1,3 @@
-var row = document.createElement('tr');
-var select = document.createElement('select');
-select.classList.add('form-select','w-50');
-select.setAttribute('name','saccoId');
-
-var td1 = document.createElement('td');
-var td2 = document.createElement('td');
-var td3 = document.createElement('td');
-td3.classList.add('d-flex','justify-content-between');
-
-var button = document.createElement('button');
-button.setAttribute('type','submit');
-button.classList.add('btn','btn-light');
-button.textContent='Apply Changes';
-
-
 const cardTitle = document.querySelector('.card-title');
 
 const body = document.querySelector('#infos');
@@ -23,68 +7,92 @@ const urlBimbo = "http://localhost:9013/api/bimbi";
 const urlSacchi = "http://localhost:9013/api/sacchi";
 
 
-function cardNameValue(){
-    cardTitle.textContent=sessionStorage.getItem('username');
+function cardNameValue() {
+    cardTitle.textContent = sessionStorage.getItem('username');
 }
 
+function newFunction(row) {
+    var select = document.createElement('select');
+    select.classList.add('form-select', 'w-50');
+    select.setAttribute('name', 'saccoId');
+
+    var td3 = document.createElement('td');
+    td3.classList.add('d-flex','justify-content-between')
+
+    var button = document.createElement('button');
+    button.setAttribute('type', 'submit');
+    button.classList.add('btn', 'btn-light');
+    button.textContent = 'Apply Changes';
+
+    fetch(urlSacchi)
+        .then(data => {
+            return data.json();
+        })
+        .then(resp => {
+            resp.forEach(element => {
+
+                var option = document.createElement('option');
+                var nome = element.nome;
+                var id = element.id;
+
+                option.textContent = nome + "-" + id;
+                option.setAttribute('value', id);
+
+                select.appendChild(option);
+
+                td3.appendChild(select);
+                td3.appendChild(button);
+                row.appendChild(td3);
+
+
+            });
+
+
+        });
+}
 
 function fillTable() {
     fetch(urlBimbo)
-    .then(data => { return data.json(); })
-    .then(resp => {
-        resp.forEach(element => {
-            
-            td1.textContent = element.nome;
-            var id = document.createElement('input');
-            id.setAttribute('value',element.id);
-            id.setAttribute('type','hidden');
-            id.setAttribute('name','bimboId')
-            td1.appendChild(id);
-            
-            element.doni.forEach(element => {
-                var nomeDono = element.nome;
-                td2.textContent = nomeDono;
-            });
-            
-            
-            
-        });
-        
-        
-        
-        fetch(urlSacchi)
-        .then(data => { return data.json(); })
+        .then(data => {
+            return data.json();
+        })
         .then(resp => {
-                resp.forEach(element => {
+            resp.forEach(element => {
+                var row = document.createElement('tr');
+                var td1 = document.createElement('td');
+                td1.textContent = element.nome;
+                var id = document.createElement('input');
+                id.setAttribute('value', element.id);
+                id.setAttribute('type', 'hidden');
+                id.setAttribute('name', 'bimboId')
+                td1.appendChild(id);
+                var td2 = document.createElement('td');
 
-                    var option = document.createElement('option');
-                    
-                    var nome = element.nome;
-                    var id = element.id;
-                    
-                    option.textContent = nome + "-" + id;
-                    option.setAttribute('value',id);
-                    
-                    select.appendChild(option);
-                    
-                    td3.appendChild(select);
-                    td3.appendChild(button);
+                element.doni.forEach(element => {
+                    var nomeDono = element.nome;
+                    td2.innerHTML += "-"+nomeDono+" ";
+                    row.appendChild(td1);
+                    row.appendChild(td2);
+
+
                     
                     
                 });
                 
-                
+                newFunction(row);
+
+                body.appendChild(row);
             });
-            
-            row.appendChild(td1);
-            row.appendChild(td2);
-            row.appendChild(td3);
-            
-            body.appendChild(row);
-            
-            
+
+
+
+
+
         });
 }
-    
+
+
+
+
 fillTable();
 cardNameValue();
